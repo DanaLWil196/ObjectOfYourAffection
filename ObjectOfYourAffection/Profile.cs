c5ded99cd0fdad53c8b54ffd7956b8bb07486183
Dana Wilson <danalwil@outlook.com>
2020-09-22 12:09:19 -0400

    Add project files.

---
+++ ObjectOfYourAffection/Profile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOfYourAffection
+{
+    class Profile
+    {
+        string name;
+        int age;
+        string adopted;
+        string pronouns;
+        string[] hobbies;
+
+        public Profile(string name, int age, string adopted, string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.adopted = adopted;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+
+        public string ViewProfile()
+        {
+            string bio = $"Name: {name}\nAge: {age}\nAdopted: {adopted}\nPronouns: {pronouns}";
+
+            if (this.hobbies.Length > 0)
+            {
+                bio += "\nHobbies:\n";
+                foreach (string hobby in hobbies)
+                {
+                    bio += $"- {hobby}\n";
+                }
+            }
+            return bio;
+        }
+        public void SetHobbies(string[]hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+}
---
+++ ObjectOfYourAffection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ObjectOfYourAffection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile smokey = new Profile("Mountain Wood Smoke", 11, "yes", "he/him");
+            smokey.SetHobbies(new string[]
+            {
+                "Napping on cold spots",
+                "Dealing with my sisters",
+                "Spending time with my family",
+                "Trying to be active due to my old bones",
+            });
+
+            Console.WriteLine(smokey.ViewProfile());
+        }
+
+    }
+}
